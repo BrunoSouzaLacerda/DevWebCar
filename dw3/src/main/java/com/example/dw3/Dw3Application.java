c2d4a4ea7e6607608e86f8653fd863bf5fd28fbd
BrunoSouzaLacerda <18013026@uepg.br>
2024-06-20 10:14:47 -0300

    /
    Publicando o trabalho final de desenvolvimento web

---
+++ dw3/src/main/java/com/example/dw3/Dw3Application.java
@@ -0,0 +1,13 @@
+package com.example.dw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dw3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dw3Application.class, args);
+	}
+
+}
